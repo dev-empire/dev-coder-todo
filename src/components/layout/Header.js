2f8db103a67730e17f730d73d38e883a49e7d93f
Al-amin Yususf <alaminyusuf131@gmail.com>
2020-05-19 00:20:16 +0100

    updated some files

--- src/components/layout/Header.js
+++ src/components/layout/Header.js
@@ -1,16 +1,15 @@
-import React from 'react';
+import React from 'react'
 
 export default function Header() {
   return (
     <header
       style={{
-        backgroundColor: '#333',
+        backgroundColor: '#515151',
         color: '#fff',
         padding: '10px',
         textAlign: 'center',
-      }}
-    >
+      }}>
       <h1>Todo List</h1>
     </header>
-  );
+  )
 }
