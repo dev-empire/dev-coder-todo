57e7c1e8c9ac41ffe6fe4f7c7e53bbe4d5e8d26b
Al-amin Yususf <alaminyusuf131@gmail.com>
2020-05-19 00:26:33 +0100

    made commit

--- src/App.js
+++ src/App.js
@@ -54,6 +54,7 @@ class App extends Component {
     this.setState({ todos: [...this.state.todos, newTodo] })
   }
   render() {
+    // destructin
     return (
       <div className='App'>
         <Header />
