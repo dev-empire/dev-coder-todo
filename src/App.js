99ffc2e7dc41271a539547f72464dcbba5ae7b6c
Al-amin Yususf <alaminyusuf131@gmail.com>
2020-05-19 00:11:03 +0100

    first commit

---
+++ src/App.js
@@ -0,0 +1,68 @@
+import React, { Component } from 'react';
+import './App.css';
+import Header from './components/layout/Header';
+import AddTodo from './components/AddTodo';
+import Todos from './components/Todos';
+import uuid from 'react-uuid';
+
+class App extends Component {
+  state = {
+    todos: [
+      {
+        id: uuid(),
+        title: 'Take out trash',
+        completed: false,
+      },
+      {
+        id: uuid(),
+        title: 'Take coures online',
+        completed: false,
+      },
+      {
+        id: uuid(),
+        title: 'Work on side project',
+        completed: false,
+      },
+    ],
+  };
+  markComplete = (id) => {
+    this.setState({
+      todos: this.state.todos.map((todo) => {
+        if (todo.id === id) {
+          todo.completed = !todo.completed;
+        }
+        return todo;
+      }),
+    });
+  };
+  delTodo = (id) => {
+    this.setState({
+      todos: [...this.state.todos.filter((todo) => todo.id !== id)],
+    });
+  };
+  addTodo = (title) => {
+    const newTodo = {
+      id: uuid(),
+      title,
+      completed: false,
+    };
+    this.setState({ todos: [...this.state.todos, newTodo] });
+  };
+  render() {
+    return (
+      <div className="App">
+        <Header />
+        <div className="container">
+          <AddTodo addTodo={this.addTodo} />
+          <Todos
+            todos={this.state.todos}
+            markComplete={this.markComplete}
+            delTodo={this.delTodo}
+          />
+        </div>
+      </div>
+    );
+  }
+}
+
+export default App;
---
+++ src/components/layout/Header.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+export default function Header() {
+  return (
+    <header
+      style={{
+        backgroundColor: '#333',
+        color: '#fff',
+        padding: '10px',
+        textAlign: 'center',
+      }}
+    >
+      <h1>Todo List</h1>
+    </header>
+  );
+}
