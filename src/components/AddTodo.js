590dd8840e0387ed9589eeff4484915a61ed6b08
Al-amin Yususf <alaminyusuf131@gmail.com>
2020-05-19 00:19:05 +0100

    commit made

--- src/components/AddTodo.js
+++ src/components/AddTodo.js
@@ -1,35 +1,35 @@
-import React, { Component } from 'react';
+import React, { Component } from 'react'
 
 export default class addTodo extends Component {
   state = {
     title: '',
-  };
+  }
 
-  onSubmit = (e) => {
-    e.preventDefault();
-    this.props.addTodo(this.state.title);
-    this.setState({ title: '' });
-  };
+  onSubmit = e => {
+    e.preventDefault()
+    this.props.addTodo(this.state.title)
+    this.setState({ title: '' })
+  }
 
-  onChange = (e) => this.setState({ [e.target.name]: e.target.value });
+  onChange = e => this.setState({ [e.target.name]: e.target.value })
   render() {
     return (
       <form onSubmit={this.onSubmit} style={{ display: 'flex' }}>
         <input
-          type="text"
-          name="title"
-          style={{ flex: '10', padding: '7px' }}
-          placeholder="Add Todo..."
+          type='text'
+          name='title'
+          style={{ flex: '10', padding: '8px' }}
+          placeholder='Add Todo...'
           value={this.state.title}
           onChange={this.onChange}
         />
         <input
-          type="submit"
-          value="Submit"
-          className="btn"
+          type='submit'
+          value='Submit'
+          className='btn'
           style={{ flex: '1' }}
         />
       </form>
-    );
+    )
   }
 }
